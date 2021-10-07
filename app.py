625e660105f3fdd648ae31c06af476c7ca988a67
Nooaah <noah.chtl@gmail.com>
2021-10-07 16:33:42 +0200

    Added CSV functionality. Allows us to add our files to a CSV sheet, just like the database

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
+from sqlite3.dbapi2 import Error
 from flask import Flask
 from flask import request
 from flask import flash, redirect
-from flask import jsonify
 import json
-import sys
 import zipfile
 import hashlib
 import sqlite3
 import os
-import random
-import string
 from datetime import date
+from csv import writer
+
 
 app = Flask(__name__)
 
 ###########################################
+##    Compte Rendu PDF dans ce dossier   ##
 ##            CHATELAIN  NOAH            ##
 ##             GYRE AMBROISE             ##
 ##              FISA TI-2023             ##
@@ -27,14 +27,8 @@ app = Flask(__name__)
 
 # Run : python3 app.py
 # OU
-# Suivre les étapes du README
+# Suivre les étapes Docker du README
 
-
-# Fonction qui retourne un String
-# Permettant de générer une chaîne de 6 caractères aléatoires
-def createRandomString():
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(6))
 
 def checkIfFileExistInDatabase(filename):
     conn = sqlite3.connect('database.db')
@@ -44,13 +38,14 @@ def checkIfFileExistInDatabase(filename):
     rows = cursor.fetchall()
     return len(rows) > 0
 
+
 UPLOAD_FOLDER = "fichiers_reception/"
 app.config['TEMP_ZIP_FOLDER'] = './temp_zip_folder/'
 
 
 @app.route('/extract', methods=["POST"])
 def extract():
-    #Création du fichier zip avec les fichiers aux mauvais code SHA-1
+    # Création du fichier zip avec les fichiers aux mauvais code SHA-1
     zipObj = zipfile.ZipFile('log_error_sha1_code_files.zip', 'w')
     if request.method == "POST":
         jsonCodes = json.loads(request.form["json"])
@@ -65,8 +60,8 @@ def extract():
 
         with zipfile.ZipFile(app.config['TEMP_ZIP_FOLDER'] + file.filename, "r") as zip_ref:
             zip_ref.extractall(UPLOAD_FOLDER)
-        
-        #Remove the .DS_Store and __MACOSX files for macOS
+
+        # Remove the .DS_Store and __MACOSX files for macOS
         os.remove(UPLOAD_FOLDER + file.filename.split(".")[0] + "/.DS_Store")
         os.rmdir(UPLOAD_FOLDER + "__MACOSX")
         for index, filename in enumerate(os.listdir(UPLOAD_FOLDER + file.filename.split(".")[0])):
@@ -78,21 +73,47 @@ def extract():
                     while len(block) != 0:
                         sha1sum.update(block)
                         block = source.read(2**16)
-                print("\nphoto" + str(index), os.path.join(UPLOAD_FOLDER + file.filename.split(".")[0], filename), sha1sum.hexdigest())
+                print("\nphoto" + str(index), os.path.join(UPLOAD_FOLDER +
+                      file.filename.split(".")[0], filename), sha1sum.hexdigest())
+                # Vérifie si le fichier existe déjà dans la base de donnée
+                # Si non, alors on l'ajout avec son nom, code sha1, date de validation, si le sha1 correspond
                 if checkIfFileExistInDatabase(filename) == False:
+                    print('ok')
                     conn = sqlite3.connect('database.db')
                     cursor = conn.cursor()
-                    oneRecord = [filename, sha1sum.hexdigest(), str(date.today().strftime("%d/%m/%Y")), bool(jsonCodes["photo" + str(index)] == sha1sum.hexdigest())]
-                    cursor.execute('INSERT INTO files(filename, sha1, validation_date, isOk) VALUES (?,?,?,?)', oneRecord)
+                    oneRecord = [filename, sha1sum.hexdigest(), str(date.today().strftime(
+                        "%d/%m/%Y")), bool(jsonCodes["photo" + str(index)] == sha1sum.hexdigest())]
+                    cursor.execute(
+                        'INSERT INTO files(filename, sha1, validation_date, isOk) VALUES (?,?,?,?)', oneRecord)
                     conn.commit()
                     conn.close()
+                    
+                    List=[filename, sha1sum.hexdigest(), str(date.today().strftime(
+                            "%d/%m/%Y")), bool(jsonCodes["photo" + str(index)] == sha1sum.hexdigest())]
+                    with open('files.csv', 'a') as f_object:
+                        writer_object = writer(f_object)
+                        writer_object.writerow(List)
+                        f_object.close()
+                        
+                    """
+                    with open('files.csv', 'w', newline='') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow((filename, sha1sum.hexdigest(), str(date.today().strftime(
+                            "%d/%m/%Y")), bool(jsonCodes["photo" + str(index)] == sha1sum.hexdigest())))
+                    """
+                    
+
                 if jsonCodes["photo" + str(index)] == sha1sum.hexdigest():
                     print("✅", filename, "SHA-1 Code is OK")
                 else:
-                    print("❌", filename, "SHA-1 Code is not OK ! File is added to log_error_sha1_code_files.zip")
-                    zipObj.write(os.path.join(UPLOAD_FOLDER + file.filename.split(".")[0], filename))
-                    os.remove(os.path.join(UPLOAD_FOLDER + file.filename.split(".")[0], filename))
-            except:
+                    print(
+                        "❌", filename, "SHA-1 Code is not OK ! File is added to log_error_sha1_code_files.zip")
+                    zipObj.write(os.path.join(UPLOAD_FOLDER +
+                                 file.filename.split(".")[0], filename))
+                    os.remove(os.path.join(UPLOAD_FOLDER +
+                              file.filename.split(".")[0], filename))
+            except Error:
+                print(Error)
                 break
         print('\n')
         zipObj.close()
